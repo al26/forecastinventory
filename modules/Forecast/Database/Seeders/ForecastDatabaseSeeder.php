68854eca88961d57f8f95f4d368932a9db0e805a
al26 <31477016+al26@users.noreply.github.com>
2019-03-21 22:22:48 +0700

    Forecast module created

---
+++ modules/Forecast/Database/Seeders/ForecastDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Forecast\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class ForecastDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
