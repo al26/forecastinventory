40215710aa534c01a0ba419680a7c6b3edadd0a1
al26 <31477016+al26@users.noreply.github.com>
2019-03-30 21:22:34 +0700

    hmmm

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,18 +7,18 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function administrator() {
-        $data['title'] = ucwords('admin dashboard');
+        $data['title'] = ucwords('Dasbor Admin');
         $data['breadcrumb'] = true;
         return view('layouts.admin', $data);
     }
 
     public function production() { 
-        $data['title'] = ucwords('prod dashboard'); 
+        $data['title'] = ucwords('Dasbor Produksi'); 
         return view('layouts.admin');
     }
 
     public function logistic() { 
-        $data['title'] = ucwords('log dashboard'); 
+        $data['title'] = ucwords('Dasbor Logistik'); 
         return view('layouts.admin');
     }
 }
