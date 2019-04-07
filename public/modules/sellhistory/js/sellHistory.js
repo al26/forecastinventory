3b6147f1d372941099a2eb2be0c47caba97edb33
amien <akurniawan@student.ce.undip.ac.id>
2019-04-07 13:36:12 +0700

    fix bug

--- public/modules/sellhistory/js/sellHistory.js
+++ public/modules/sellhistory/js/sellHistory.js
@@ -1,15 +1,15 @@
-getLastPeriodOfProduct = function(trigger, period, quarter, baseUri) {
-    let source = $(trigger).val();
+getLastPeriodOfProduct = function (trigger, period, quarter, baseUri) {
+	let source = $(trigger).val();
 	let url = baseUri + "/last-period-of/" + source;
-	
+
 	$.ajax({
 		type: 'get',
 		url: url,
-		success: function(response) {
+		success: function (response) {
 			$(period).val(response[0].period);
 			$(quarter).val(response[0].quarter);
 		},
-		error: function(response) {
+		error: function (response) {
 			console.log(response);
 		}
 	});
