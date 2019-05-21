01b39ac8c4cd78bc9025dd6228124a5663e3089d
al26 <31477016+al26@users.noreply.github.com>
2019-05-21 22:51:05 +0700

    update onload durung

--- modules/Inventory/Resources/assets/js/update_product.js
+++ modules/Inventory/Resources/assets/js/update_product.js
@@ -1,17 +1,27 @@
 var materialCodes = new Array();
+// var sessionMaterialCode;
+
+$(document).ready(function(){
+    // console.log(sessionMaterialCode)
+    pickMaterial(pickMaterialUrl, true);
+})
+// openModal(`http://localhost:8000/administrator/inventory/getDataMaterial`)
 function openModal(url){
-    let code = $('#btnMaterial').data('code');
+    let sessionMaterialCode = sessionStorage.getItem("sessionMaterialCode");
+    let code = (sessionMaterialCode !== null && sessionMaterialCode !== "") ? sessionMaterialCode.split(',') : $('#btnMaterial').data('code');
     $.ajax({
         type: "GET",
         url: url,
         data:{material_code:code},
         success: function(data){
+            sessionStorage.setItem("sessionMaterialCode",code);
+            sessionStorage.getItem("sessionMaterialCode");
             $('.modal-body').replaceWith(data);
             $("#mediumModal").modal('show');
         },
    });
 }
-
+// function to add material 
 function submitMaterial(url){
     $.ajaxSetup({
         headers: {
@@ -43,23 +53,44 @@ function submitMaterial(url){
 
 function selectedMaterial(code){
         materialCodes.push(code);
+        console.log("selected material",materialCodes);
 }
-function pickMaterial(url){
+
+// pickMaterial(`http://localhost:8000/administrator/inventory/getmaterialselected`)
+function pickMaterial(url, onload = false){
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('input[name="_token"]').val()
         }
     });
+    let alreadyShowed = null;
     let material = sameValue(materialCodes);
     let uniqueMaterial = Object.keys(material);
+    let sessionMaterialCode = sessionStorage.getItem("sessionMaterialCode");
+    sessionMaterialCode = (sessionMaterialCode !== null && sessionMaterialCode !== "" && uniqueMaterial.length > 0) ? sessionMaterialCode.concat(","+uniqueMaterial) : uniqueMaterial;
+    let material_code = onload ? sessionMaterialCode : uniqueMaterial;
+    if (onload) {
+        alreadyShowed = $('#formAfter').find("input[id=text-input]");
+        console.log(alreadyShowed);
+        if(material_code.length <= 0 && sessionMaterialCode.length > 0) {
+            let diff = arr_diff(alreadyShowed, sessionMaterialCode);
+            if(diff.length > 0) {
+                material_code = diff;
+                sessionMaterialCode = materialCodes;
+            }
+        }
+    }
+    console.log(material_code)
     $.ajax({
-        type:"post",
+        type:"POST",
         url:url,
-        data:{material_code:uniqueMaterial},
-        success:function(data){
-            materialCodes = [];          
-            
-            $('#formAfter').replaceWith(data);
+        data:{material_code:material_code,data_action:"update"},
+        success:function(res){
+            console.log('data dari pick',res)
+            materialCodes = [];
+            sessionStorage.removeItem("sessionMaterialCode");
+            sessionStorage.setItem("sessionMaterialCode", sessionMaterialCode);
+            $('#formAfter').append(res.html);
             $('button[type="submit"]').removeAttr("hidden");
             $('button[type="reset"]').removeAttr("hidden");
         },
@@ -88,4 +119,27 @@ function remove(num, obj){
 function removeInputMaterial(id){
     let param = '#inputMaterial'+id; 
     $(param).remove().fadeOut("slow");
+}
+
+function arr_diff (a1, a2) {
+
+    var a = [], diff = [];
+
+    for (var i = 0; i < a1.length; i++) {
+        a[a1[i]] = true;
+    }
+
+    for (var i = 0; i < a2.length; i++) {
+        if (a[a2[i]]) {
+            delete a[a2[i]];
+        } else {
+            a[a2[i]] = true;
+        }
+    }
+
+    for (var k in a) {
+        diff.push(k);
+    }
+
+    return diff;
 }
---
+++ modules/Inventory/Resources/views/include/form-update-material.blade.php
@@ -0,0 +1,21 @@
+
+    <?php $kode = array();?>
+     @foreach ($datamaterial as $item => $value)
+    <?php 
+      array_push($kode,$value->material_code);
+      $code = json_encode($kode); 
+    ?>
+    <div class="row form-group" id="inputMaterial{{$value->material_code}}"> 
+            <div class="col col-md-2">
+                <label for="select" class=" form-control-label">{{$value->material_name}}</label>
+            </div>
+            <div class="col-12 col-md-3">
+                <input type="text" id="text-input" onkeypress="javascript:return isNumberKey(event);" value="" required placeholder="{{$value->unit}}" name="{{$value->material_code}}" class="form-control" onkeyup="setPickedMaterialValues()">
+                <small class="form-text text-danger">{{ $errors->error->first('nama_product') }}</small>
+                {{-- <small class="form-text text-muted">Kolom ini untuk tanggal pembelian bahan baku</small> --}}
+            </div>
+            <div class="col-12 col-md-3">
+            <button type="button" onclick="removeInputMaterial(`{{$value->material_code}}`)" class="btn btn-primary btn-sm">Hapus</button>
+            </div>
+        </div>            
+    @endforeach
